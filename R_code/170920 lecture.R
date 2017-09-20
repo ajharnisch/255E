3572b5b94ea2187d63dded459e705fe4c39d6036
ajharnisch <31583681+ajharnisch@users.noreply.github.com>
2017-09-20 16:06:02 -0700

    suamrry
    commint

---
+++ R_code/170920 lecture.R
@@ -0,0 +1,24 @@
+# levels function meant to reorder with in the argument for the levels. for example moving one to a different area would have placed int in a different place on the graph
+
+
+# after back ticks in chunks cannot have spaces because it will interperate it as a following command. you do not need labels for chunkc. 
+#outline in markdown forexmaple you can cklick and be taken to the heading. 
+
+#Quiz should be backticks. working directory is the folder with the rdata
+
+#clark - , Bagerly, 
+
+#github - place to share codes. the git program can be used to see what changes can be made. you can keep track of exactly what changes you have made
+# track changes you can make a commit statemnt. 
+
+#creating a git repository
+# md files are mark down files
+# note the green lines under repository
+#get commint 
+# information can be replicated on the gethub website. 
+#get hub can host websites, it will match what is on the repository. 
+
+#branches can be created changes in the rmd code if you are working on a side branch hte master version. If you think the 
+#cade may radically chaneg. 
+
+#multiple changes can be made can be changed by selecting boxes. 
