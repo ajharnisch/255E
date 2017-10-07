9ba28be8aa82bade836f554650a4312cd881879f
ajharnisch <31583681+ajharnisch@users.noreply.github.com>
2017-10-07 08:58:01 -0700

    Practice for homework week 7

---
+++ R_code/171004 practice.R
@@ -0,0 +1,12 @@
+#regular expressions pull chuncks of text out of text feilds. 
+#pattern =  - got back rows with accoutnant. this was the most absic regualr expression. 
+#format (as.date(dat$))
+
+#patter= "[a-Z]" - finds uppercasse and lower case. 
+
+
+df = read.csv('../data/california-state-university-2015.csv')
+
+
+
+df = read.csv('../data/california-state-university-2015.csv')
